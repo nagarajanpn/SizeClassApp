e4c23046b96d20ab91c2a2759a094f22dd270c0c
Daniel Chong <wflogin@daniels-imac21.local>
2018-12-31 10:14:10 +0530

    Initial Commit

---
+++ SizeClassApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClassApp
+//
+//  Created by wflogin on 31/12/18.
+//  Copyright © 2018 wflogin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
