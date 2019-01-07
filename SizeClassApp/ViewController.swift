efcbcd712d4fa28e681dd65b248ee340ea8babd6
gowtham <30319107+gowthamSakthi@users.noreply.github.com>
2019-01-07 14:26:54 +0530

    Update ViewController.swift

--- SizeClassApp/ViewController.swift
+++ SizeClassApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var myname:String = "Nagarajan"
+    var myname:String = "Nagarajan ios developer"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
